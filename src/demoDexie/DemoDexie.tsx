ea05c03dcc872675b4f0acce0150500d090d7b8a
Tony Hallett <tonyhallett74@gmail.com>
2025-11-25 19:27:52 +0000

    demo outbound

--- src/demoDexie/DemoDexie.tsx
+++ src/demoDexie/DemoDexie.tsx
@@ -3,42 +3,40 @@ import { dexieFactory } from "./dexieFactory";
 import { tableBuilder } from "./tableBuilder";
 
 interface DexieDataItem {
-  id: number;
-  other: number;
-}
-class X implements DexieDataItem {
-  constructor(public id: number) {}
-  other: number = 0;
-  method() {}
+  value: number;
 }
 
 const db = dexieFactory(
   1,
   {
-    data: tableBuilder<DexieDataItem>().autoIncrement("id").build(),
+    hiddenAuto: tableBuilder<DexieDataItem>().hiddenAuto().build(),
+    hiddenExplicit: tableBuilder<DexieDataItem>().hiddenExplicit().build(),
   },
-  "DemoDexieAuto"
+  "DemoDexieOutbound"
 );
 
-db.data.hook("updating", function (modifications, primKey, obj, transaction) {
-  this.onsuccess = function (updateObject) {
-    //
-  };
-  this.onerror = function (e) {};
-});
-db.data.hook("deleting", function (primKey, obj, transaction) {
+db.hiddenAuto.hook(
+  "updating",
+  function (modifications, primKey, obj, transaction) {
+    this.onsuccess = function (updateObject) {
+      //
+    };
+    this.onerror = function (e) {};
+  }
+);
+db.hiddenAuto.hook("deleting", function (primKey, obj, transaction) {
   this.onsuccess = function () {
     var args = arguments;
   };
   this.onerror = function (e) {};
 });
-db.data.hook("creating", function (primKey, obj, transaction) {
+db.hiddenAuto.hook("creating", function (primKey, obj, transaction) {
   this.onsuccess = function (key) {
     //
   };
   this.onerror = function (e) {};
 });
-db.data.hook("reading", function (value) {
+db.hiddenAuto.hook("reading", function (value) {
   return value;
 });
 
@@ -46,21 +44,17 @@ export const DemoDexie = () => {
   return (
     <Button
       onClick={async () => {
-        await db.data.clear();
-        // if it does not include number in auto pkey type then must supply...
-        // or ensure that it is or is a union that includes number
-        // otherwise should use the other format
-
-        // otherwise is optional
-        const newItem = { other: 1 };
-        await db.data.add(new X(123));
-        // allows adding with pkey
-        // const newItem2: DexieDataItem = { other: 1, id: 2 };
-        //const newItemNotAllowed = { other: 1, id: undefined };
-        //db.data.add(newItemNotAllowed);
-        await db.data.add(newItem);
-        // const withPrimaryKey = await db.data.addObject(newItem);
-        // const received = await db.data.get(2);
+        await db.hiddenAuto.clear();
+        await db.hiddenExplicit.clear();
+        const addAutoItem: DexieDataItem = { value: 1 };
+        const addAutoItem2: DexieDataItem = { value: 2 };
+        const hiddenAutoKey = await db.hiddenAuto.add(addAutoItem);
+        const hiddenAutoKey2 = await db.hiddenAuto.add(addAutoItem2, 100);
+        const addExplicitItem: DexieDataItem = { value: 1 };
+        const hiddenExplicitKey = await db.hiddenExplicit.add(
+          addExplicitItem,
+          1
+        );
       }}
     >
       Demo Dexie
